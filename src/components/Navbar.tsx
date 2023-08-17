19cc3d31e29e1ffb5bd4b512083c83acce4596f7
NwaguChima <nwaguchima48@gmail.com>
2023-08-17 02:38:36 +0100

    added logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,22 @@
+import Link from 'next/link';
+import { Icons } from './Icons';
+
 const Navbar = () => {
-  return <div></div>;
+  return (
+    <div className="fixed top-0 inset-x-0 h-fit bg-zinc-100 border-b border-zinc-300 z-[10] py-2">
+      <div className="container max-w-7xl h-full mx-auto flex items-center justify-between gap-2">
+        {/* logo */}
+        <Link href="/" className="flex gap-2 items-center">
+          <Icons.logo className="w-10 h-10" />
+          <p className="hidden text-zinc-700 text-sm font-medium md:block">
+            LeagueReddit
+          </p>
+        </Link>
+
+        {/* search bar */}
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
