55a3d906a3157d6bda1b5a17d9640f3aa9790bb1
NwaguChima <nwaguchima48@gmail.com>
2023-08-18 08:49:26 +0100

    intercepted route for signup

--- src/app/@authModal/(.)sign-in/page.tsx
+++ src/app/@authModal/(.)sign-in/page.tsx
@@ -2,9 +2,7 @@ import CloseModal from '@/components/CloseModal';
 import SignIn from '@/components/SignIn';
 import React from 'react';
 
-interface pageProps {}
-
-const page: React.FC<pageProps> = ({}) => {
+const page = () => {
   return (
     <div className="fixed inset-0 bg-zinc-900/20 z-10">
       <div className="container flex items-center h-full max-w-lg mx-auto">
