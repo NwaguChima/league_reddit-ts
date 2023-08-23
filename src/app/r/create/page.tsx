4db479a15bcab36fed31eb4fd78b8daf43af0066
NwaguChima <nwaguchima48@gmail.com>
2023-08-23 05:45:25 +0100

    setup axios errors

--- src/app/r/create/page.tsx
+++ src/app/r/create/page.tsx
@@ -4,8 +4,9 @@ import { Button } from '@/components/ui/Button';
 import { Input } from '@/components/ui/Input';
 import { useRouter } from 'next/navigation';
 import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
 import { CreateSubredditPayload } from '@/lib/validators/subreddit';
+import { toast } from '@/hooks/use-toast';
 
 const Page = () => {
   const [input, setInput] = useState<string>('');
@@ -19,6 +20,34 @@ const Page = () => {
 
       const { data } = await axios.post('/api/subreddit', payload);
       return data as string;
+    },
+    onError: (err) => {
+      if (err instanceof AxiosError) {
+        if (err.response?.status === 409) {
+          return toast({
+            title: 'Subreddit already exists',
+            description: 'Please choose a different subreddit name',
+            variant: 'destructive',
+          });
+        }
+
+        if (err.response?.status === 422) {
+          return toast({
+            title: 'Invalid subreddit name',
+            description:
+              'please choose a name between 3 and 21 characters long',
+            variant: 'destructive',
+          });
+        }
+
+        if (err.response?.status === 401) {
+          return toast({
+            title: '',
+            description: 'Please choose a different subreddit name',
+            variant: 'destructive',
+          });
+        }
+      }
     },
   });
 
@@ -53,7 +82,11 @@ const Page = () => {
           <Button variant="subtle" onClick={() => router.back()}>
             Cancel
           </Button>
-          <Button isLoading={isLoading} disabled={input.length === 0}>
+          <Button
+            isLoading={isLoading}
+            disabled={input.length === 0}
+            onClick={() => createCommunity()}
+          >
             Create Community
           </Button>
         </div>
