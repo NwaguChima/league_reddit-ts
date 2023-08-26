f42004108504d606e4a514a39f3dde279c280583
NwaguChima <nwaguchima48@gmail.com>
2023-08-26 13:52:32 +0100

    added dl info on the slug layout page

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,4 +1,7 @@
 import { getAuthSession } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { format } from 'date-fns';
+import { notFound } from 'next/navigation';
 
 const Layout = async ({
   children,
@@ -11,6 +14,45 @@ const Layout = async ({
 }) => {
   const session = await getAuthSession();
 
+  const subreddit = await db.subreddit.findFirst({
+    where: {
+      name: slug,
+    },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+        },
+      },
+    },
+  });
+
+  const subscription = !session?.user
+    ? undefined
+    : await db.subscription.findFirst({
+        where: {
+          subreddit: {
+            name: slug,
+          },
+          user: {
+            id: session.user.id,
+          },
+        },
+      });
+
+  const isSubscribed = !!subscription;
+
+  if (!subreddit) return notFound();
+
+  const memberCount = await db.subscription.count({
+    where: {
+      subreddit: {
+        name: slug,
+      },
+    },
+  });
+
   return (
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
       <div>
@@ -22,8 +64,36 @@ const Layout = async ({
           {/* info sidebar */}
           <div className="hidden md:block overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
             <div className="px-6 py-4">
-              <p className="font-semibold py-3">About r/</p>
+              <p className="font-semibold py-3">About r/{subreddit.name}</p>
             </div>
+
+            <dl className="divide-y divide-gray-100 px-6 py-4 text-sm leading-6 bg-white">
+              <div className="flex justify-between gap-x-4 py-3">
+                <dt className="text-gray-500">Created</dt>
+                <dd className="text-gray-700">
+                  <time dateTime={subreddit.createdAt.toISOString()}>
+                    {format(subreddit.createdAt, 'MMM d, yyyy')}
+                  </time>
+                </dd>
+              </div>
+
+              <div className="flex justify-between gap-x-4 py-3">
+                <dt className="text-gray-500">Members</dt>
+                <dd className="text-gray-700">
+                  <div className="text-gray-900">{memberCount}</div>
+                </dd>
+              </div>
+
+              {subreddit.creatorId === session?.user?.id ? (
+                <div className="flex justify-between gap-x-4 py-3">
+                  <p className="text-gray-500">You created this community</p>
+                </div>
+              ) : null}
+
+              {subreddit.creatorId !== session?.user?.id ? (
+                <SubscribeLeaveToggle isSubscribed={isSubscribed} />
+              ) : null}
+            </dl>
           </div>
         </div>
       </div>
