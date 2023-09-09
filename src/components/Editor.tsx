11d4a50d5a9e949dcd4e33d28685c9f9b7dec88f
NwaguChima <nwaguchima48@gmail.com>
2023-09-09 17:28:45 +0100

    setup text area auto size

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,20 @@
+'use client'
+
 import React from 'react'
+import TextareaAutosize from 'react-textarea-autosize';
 
 interface EditorProps {
 
 }
 
 const Editor: React.FC<EditorProps> = ({}) => {
- return <div>Editor</div>
+ return <div className='w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200'>
+    <form id='subreddit-post-form' className='w-fit' onSubmit={() => {}}>
+        <div className="prose prose-stone dark:prose-invert">
+            <TextareaAutosize placeholder='Title' className='w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none' />
+        </div>
+    </form>
+ </div>
 }
 
 export default Editor
