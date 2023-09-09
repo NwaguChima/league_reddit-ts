02b0b78255f36df3d31001737dccff31ee58ee50
NwaguChima <nwaguchima48@gmail.com>
2023-09-09 17:23:46 +0100

    setup editor

--- src/app/r/[slug]/submit/page.tsx
+++ src/app/r/[slug]/submit/page.tsx
@@ -1,3 +1,4 @@
+import Editor from '@/components/Editor';
 import { Button } from '@/components/ui/Button';
 import { db } from '@/lib/db';
 import { notFound } from 'next/navigation';
@@ -29,7 +30,7 @@ const page = async ({ params }: PageProps) => {
     </div>
 
     {/* Form */}
-    {/* <Editor /> */}
+    <Editor />
 
     <div className="w-full flex justify-end">
         <Button type='submit' className='w-full' form='subreddit-post-form'>Post</Button>
