09cfd53c3bb9df610822c22a4fe941da10978033
NwaguChima <nwaguchima48@gmail.com>
2023-09-09 18:35:30 +0100

    completed editor functionality

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import TextareaAutosize from 'react-textarea-autosize';
 import { useForm } from 'react-hook-form';
 import { PostCreationRequest, PostValidator } from '@/lib/validators/post';
 import { zodResolver } from '@hookform/resolvers/zod';
 import type EditorJS from '@editorjs/editorjs';
+import { uploadFiles } from '@/lib/uploadthing';
 
 interface EditorProps {
   subredditId: string;
@@ -26,6 +27,14 @@ const Editor: React.FC<EditorProps> = ({ subredditId }) => {
   });
 
   const ref = useRef<EditorJS>();
+  const [isMounted, setIsMounted] = useState(false);
+  const _titleRef = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setIsMounted(true);
+    }
+  }, []);
 
   const initializeEditor = useCallback(async () => {
     const EditorJS = (await import('@editorjs/editorjs')).default;
@@ -61,23 +70,68 @@ const Editor: React.FC<EditorProps> = ({ subredditId }) => {
             class: ImageTool,
             config: {
               uploader: {
-                async uploadByFile(file: File) {},
+                async uploadByFile(file: File) {
+                  const [res] = await uploadFiles([file], 'imageUploader');
+
+                  return {
+                    success: 1,
+                    file: {
+                      url: res.fileUrl,
+                    },
+                  };
+                },
               },
             },
           },
+
+          list: List,
+          code: Code,
+          inlineCode: InlineCode,
+          table: Table,
+          embed: Embed,
         },
       });
     }
   }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      await initializeEditor();
+
+      setTimeout(() => {
+        _titleRef.current?.focus();
+      }, 0);
+    };
+
+    if (isMounted) {
+      init();
+
+      return () => {
+        ref.current?.destroy();
+        ref.current = undefined;
+      };
+    }
+  }, [isMounted, initializeEditor]);
+
+  const { ref: titleRef, ...rest } = register('title');
 
   return (
     <div className="w-full p-4 bg-zinc-50 rounded-lg border border-zinc-200">
       <form id="subreddit-post-form" className="w-fit" onSubmit={() => {}}>
         <div className="prose prose-stone dark:prose-invert">
           <TextareaAutosize
+            ref={(e) => {
+              titleRef(e);
+
+              // @ts-ignore
+              _titleRef.current = e;
+            }}
+            {...rest}
             placeholder="Title"
             className="w-full resize-none appearance-none overflow-hidden bg-transparent text-5xl font-bold focus:outline-none"
           />
+
+          <div id="editor" className="min-h-[500px]" />
         </div>
       </form>
     </div>
