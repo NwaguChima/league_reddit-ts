4daf667c749f332de9f5c4362f2669a743fc3aeb
NwaguChima <nwaguchima48@gmail.com>
2023-09-16 21:47:28 +0100

    completed the vote route and caching implementation

--- src/app/api/subreddit/post/vote/route.ts
+++ src/app/api/subreddit/post/vote/route.ts
@@ -1,7 +1,9 @@
 import { getAuthSession } from '@/lib/auth';
 import { db } from '@/lib/db';
+import { redis } from '@/lib/redis';
 import { PostVoteValidator } from '@/lib/validators/vote';
 import type { CachedPost } from '@/types/redis';
+import { z } from 'zod';
 
 const CACHE_AFTER_UPVOTES = 1;
 
@@ -80,7 +82,49 @@ export async function PATCH(req: Request) {
           currentVote: voteType,
           createdAt: post.createdAt,
         };
+
+        await redis.hset(`post:${post.id}`, cachePayload);
       }
+
+      return new Response('OK');
     }
-  } catch (error) {}
+
+    await db.vote.create({
+      data: {
+        type: voteType,
+        userId: session.user.id,
+        postId,
+      },
+    });
+
+    // recount votes
+    const votesAmt = post.votes.reduce((acc, vote) => {
+      if (vote.type === 'UP') return acc + 1;
+      if (vote.type === 'DOWN') return acc - 1;
+      return acc;
+    }, 0);
+
+    if (votesAmt >= CACHE_AFTER_UPVOTES) {
+      const cachePayload: CachedPost = {
+        authorUsername: post.author.username ?? '',
+        content: JSON.stringify(post.content),
+        id: post.id,
+        title: post.title,
+        currentVote: voteType,
+        createdAt: post.createdAt,
+      };
+
+      await redis.hset(`post:${post.id}`, cachePayload);
+    }
+
+    return new Response('OK');
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return new Response('Invalid request data passed', { status: 422 });
+    }
+
+    return new Response('Could not register your vote, please try again', {
+      status: 500,
+    });
+  }
 }
