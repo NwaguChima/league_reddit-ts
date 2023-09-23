caa768f340a9d886c4dfa6af09505b7f4861e365
NwaguChima <nwaguchima48@gmail.com>
2023-09-23 12:25:56 +0100

    fixed auth issues

--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -1,4 +1,5 @@
 import SubscribeLeaveToggle from '@/components/SubscribeLeaveToggle';
+import ToFeedButton from '@/components/ToFeedButton';
 import { buttonVariants } from '@/components/ui/Button';
 import { getAuthSession } from '@/lib/auth';
 import { db } from '@/lib/db';
@@ -44,8 +45,7 @@ const Layout = async ({
         },
       });
 
-      
-      const isSubscribed = !!subscription;
+  const isSubscribed = !!subscription;
 
   if (!subreddit) return notFound();
 
@@ -60,7 +60,7 @@ const Layout = async ({
   return (
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
       <div>
-        {/* button to take us back TODO: */}
+        <ToFeedButton />
 
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
           <div className="flex flex-col col-span-2 space-y-6">{children}</div>
@@ -102,11 +102,12 @@ const Layout = async ({
                 />
               ) : null}
 
-              <Link className={buttonVariants({
-                variant: 'outline',
-                className: 'w-full mb-6',
-              })}
-              href={`r/${slug}/submit`}
+              <Link
+                className={buttonVariants({
+                  variant: 'outline',
+                  className: 'w-full mb-6',
+                })}
+                href={`r/${slug}/submit`}
               >
                 Create Post
               </Link>
