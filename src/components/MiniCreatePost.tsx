b4b48c621795bffa62b646acca5c41f43b3f37b0
NwaguChima <nwaguchima48@gmail.com>
2023-10-01 02:47:33 +0100

    fixed position of check

--- src/components/MiniCreatePost.tsx
+++ src/components/MiniCreatePost.tsx
@@ -27,7 +27,7 @@ const MiniCreatePost: React.FC<MiniCreatePostProps> = ({ session }) => {
             }}
           />
 
-          <span className="absolute bottom-0 right-0 rounded-full w-3 h-3 bg-green-500 outline-2 outline-white" />
+          <span className="absolute top-8 right-0 rounded-full w-3 h-3 bg-green-500 outline-2 outline-white" />
         </div>
 
         <Input
